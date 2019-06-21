95af06d97a5d0d9a817ec35df8213abbc934af74
Tristan Hermanns <s8trherm@stud.uni-saarland.de>
2019-06-21 11:36:27 +0200

    maybe tests are fixed?

---
+++ src/tetris/autoplay/APlayerImplementation.java
@@ -0,0 +1,37 @@
+package tetris.autoplay;
+
+import tetris.game.GameObserver;
+
+public class APlayerImplementation implements GameObserver, AutoPlayer {
+
+	@Override
+	public Move getMove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void rowsCompleted() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void piecePositionChanged() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pieceLanded() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void gameOver() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
